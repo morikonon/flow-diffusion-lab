b7b3ae9aed53341f6fe65354d0293cf432ce96cd
Mukhamedali Daniyaruly <153483615+morikonon@users.noreply.github.com>
2026-05-03 16:05:21 +0500

    add tools.py

---
+++ models/tools.py
@@ -0,0 +1,23 @@
+import torch
+
+def get_patch_position_embedding(pos_emb_dim, grid_size, device):
+	grid_size_h, grid_size_w = grid_size
+	grid_h = torch.arange(grid_size_h, dtype=torch.float32, device=device)
+	grid_w = torch.arange(grid_size_w, dtype=torch.float32, device=device)
+
+	grid = torch.meshgrid(grid_h, grid_w, indexing="ij")
+	grid = torch.stack(grid, dim=0)
+
+	grid_h_positions = grid[0].reshape(-1)
+	grid_w_positions = grid[1].reshape(-1)
+
+	factor = 10000 ** (torch.arange(0, pos_emb_dim // 4, dtype=torch.float32, device=device) / (pos_emb_dim // 4))
+
+	grid_h_emb = grid_h_positions[:, None] / factor
+	grid_w_emb = grid_w_positions[:, None] / factor
+
+	grid_h_emb = torch.cat([torch.sin(grid_h_emb), torch.cos(grid_h_emb)], dim=1)
+	grid_w_emb = torch.cat([torch.sin(grid_w_emb), torch.cos(grid_w_emb)], dim=1)
+
+	pos_emb = torch.cat([grid_h_emb, grid_w_emb], dim=-1)
+	return pos_emb
